2043dc1a474e1c7b38b8f8069ba3da8d0d7b19a3
Ahmed Nagy <ahmed.nagy@souhoola.com>
2024-06-05 01:43:57 +0300

    Test

---
+++ Sources/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ahmed Nagy on 05/06/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("This a SwiftUI View From the Souhoola Design System Package")
+    }
+}
